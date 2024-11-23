624070891ab04fac8a53f64ed15be1948c7b9197
dreadnoughtB3 <134913974+dreadnoughtB3@users.noreply.github.com>
2024-11-23 19:54:19 +0900

    for production

--- main.py
+++ main.py
@@ -8,9 +8,12 @@ from scripts.diffLogger import logging_delete, logging_edit
 from modules.get_datetime import get_japan_current_time
 from scripts.stock_make import stock_make
 from scripts.trade_make import trade_make
+from scripts.check_file_exist import check_stock_data
+
+from server import server_thread
 
 intents = discord.Intents.all()
-bot = commands.Bot(command_prefix="$", intents=intents)
+bot = commands.Bot(command_prefix="?", intents=intents)
 load_dotenv(verbose=True)
 TOKEN = os.environ.get("TOKEN")
 
@@ -23,16 +26,16 @@ async def loop():
     if now == "00:00" or now == "12:00":
         # 交易投稿
         trade_make()
-        channel = bot.get_channel(1309793637046943795)  # Stella
+        channel = bot.get_channel(992623803291144244)  # Stella
         await channel.send(file=discord.File("outputs/trade.jpg"))
     if now == "00:00":
         embed_f, embed_n = stock_make()
         # ファンタジア株
-        channel = bot.get_channel(1309793637046943795)  # Stella
+        channel = bot.get_channel(992086009061843056)  # Stella
         await channel.send(file=discord.File("outputs/stock_f.jpg"))
         await channel.send(embed=embed_f)
         # ノクターン株
-        channel = bot.get_channel(1309793637046943795)  # Stella
+        channel = bot.get_channel(946667883168153610)  # Stella
         await channel.send(file=discord.File("outputs/stock_n.jpg"))
         await channel.send(embed=embed_n)
 
@@ -41,7 +44,8 @@ async def loop():
 async def on_ready():
     """Bot起動時に実行される処理"""
     print(f"{bot.user}: 起動完了")
-    # loop.start()
+    check_stock_data() # 株価用CSVデータが存在しない場合は初期化する
+    loop.start()
 
 
 @bot.event
@@ -53,7 +57,7 @@ async def setup_hook():
 
 @bot.event
 async def on_message_edit(before, after):
-    if before.author.id == 1088213601531211857:
+    if before.author.id == 344181098818830338:
         channel = bot.get_channel(1308387319002169404)
         send_message = logging_edit(before, after)
         await channel.send(send_message)
@@ -68,4 +72,5 @@ async def on_message_delete(message):
 
 
 if __name__ == "__main__":
+    server_thread()
     bot.run(TOKEN)  # Botを起動
